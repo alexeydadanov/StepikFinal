fe46c7270b56f7a6265eb41d634a71b27ff654b7
BigBang <zagg12345@yandex.ru>
2019-11-19 15:13:54 +0300

    Переходы между страницами

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,10 @@ from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+        
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         assert ('login' in self.browser.current_url, True), "No 'Login' in url"
